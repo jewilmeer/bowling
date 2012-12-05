6d05a471006a2e5cc678bffb4bb1cb7b87c7d23f
Jan-Willem van der Meer <mail@jewilmeer.nl>
2012-12-05 22:47:04 +0100

    refactor specs, simplify them

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -9,52 +9,25 @@ describe BowlingGame do
     it { should eql 0 }
   end
 
-  context "when throwing 1 and 7" do
-    let(:score_string) { "17------------------" }
-    it { should eql 8 }
-  end
-
   context "when throwing all ones" do
     let(:score_string) { "11111111111111111111" }
     it { should eql 20 }
   end
 
+  context "All strikes" do
+    let(:score_string) { "XXXXXXXXXXX" }
+    it { should eql 300 }
+  end
+
   describe "spare" do
-    context "with last turn nine and spare" do
+    context "roling a 9, spare and 5" do
       let(:score_string) { "----9/5-------------" }
       it { should eql 20 }
     end
 
-    context "with last turn 3 and spare" do
-      let(:score_string) { "----5/7-------------" }
-      it { should eql 24 }
+    context "roling a 5, spare and 8" do
+      let(:score_string) { "----5/8-------------" }
+      it { should eql 26 }
     end
-
-    context "last throw is a spare" do
-      let(:score_string) { "------------------3/4" }
-      it { should eql 18 }
-    end
-  end
-
-  describe "strike" do
-    context "one strike without luck" do
-      let(:score_string) { "--X----------------"}
-      it { should eql 10 }
-    end
-
-    context "one strike with bonus" do
-      let(:score_string) { "--X12--------------"}
-      it { should eql 16 }
-    end
-
-    context "two consecutive strikes" do
-      let(:score_string) { "--XX--------------" }
-      it { should eql 30 }
-    end
-  end
-
-  describe "spare and strike combined" do
-    let(:score_string) { "--4/X--------------" }
-    it { should eql 30 }
   end
 end
